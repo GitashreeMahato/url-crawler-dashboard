29f3eec75269d467f98872bfb5b5983449947486
GITASHREE MAHATO <mahato.gitashree@gmail.com>
2025-07-06 22:35:44 +0530

    feat: use dynamic port from .env and config loader

--- backend/main.go
+++ backend/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// 1. Load environment variables from .env
+	LoadEnv()
+	//2. Print a confirmation
+	fmt.Println("Config loaded. Running server on port:", AppConfig.Port)
+	// 3. start Gin server
 	router := gin.Default()
 
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "pong"})
 	})
-
-	router.Run(":8080")
+	// 4. Use port from config
+	router.Run(":" + AppConfig.Port)
 }
