a6b618b3b44b9887ba46491afb223ad1a7965195
GITASHREE MAHATO <mahato.gitashree@gmail.com>
2025-07-07 22:20:00 +0530

    feat: add POST /urls route inside main.go

--- backend/main.go
+++ backend/main.go
@@ -18,6 +18,26 @@ func main() {
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "pong"})
 	})
+	router.POST("/urls", CreateUrl)
 	// 4. Use port from config
 	router.Run(":" + AppConfig.Port)
+}
+func CreateUrl(c *gin.Context) {
+	var newUrl Url
+
+	// Bind JSON to struct
+	if err := c.ShouldBindJSON(&newUrl); err != nil {
+		c.JSON(400, gin.H{"error": "Invalid JSON"})
+		return
+	}
+
+	newUrl.Status = "queued"
+
+	// Save to DB
+	if err := DB.Create(&newUrl).Error; err != nil {
+		c.JSON(500, gin.H{"error": "Failed to save URL"})
+		return
+	}
+
+	c.JSON(201, newUrl)
 }
